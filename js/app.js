41b9c64bb140a0efc365e298ae8ee7374a52194b
Klodovsky <khaled.benhassen@polytechnicien.tn>
2022-03-23 13:43:37 -0600

    reCAPTCHA

--- js/app.js
+++ js/app.js
@@ -8,14 +8,67 @@ $(function () {
 function preloader() {
     $('.preloader').delay(0).fadeOut();
 };
+function mainSlider() {
+    var BasicSlider = $('.slider-active');
+    BasicSlider.on('init', function (e, slick) {
+        var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
+        doAnimations($firstAnimatingElements);
+    });
+    BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+        var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+        doAnimations($animatingElements);
+    });
+    BasicSlider.slick({
+        autoplay: true,
+        autoplaySpeed: 10000,
+        dots: false,
+        fade: true,
+        arrows: false,
+        responsive: [
+            { breakpoint: 767, settings: { dots: false, arrows: false } }
+        ]
+    });
 
+    function doAnimations(elements) {
+        var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        elements.each(function () {
+            var $this = $(this);
+            var $animationDelay = $this.data('delay');
+            var $animationType = 'animated ' + $this.data('animation');
+            $this.css({
+                'animation-delay': $animationDelay,
+                '-webkit-animation-delay': $animationDelay
+            });
+            $this.addClass($animationType).one(animationEndEvents, function () {
+                $this.removeClass($animationType);
+            });
+        });
+    }
+}
+function aosAnimation() {
+    AOS.init({
+        duration: 1000,
+        mirror: true,
+        once: true,
+        disable: 'mobile',
+    });
+}
+function wowAnimation() {
+    var wow = new WOW({
+        boxClass: 'wow',
+        animateClass: 'animated',
+        offset: 0,
+        mobile: false,
+        live: true
+    });
+    wow.init();
+}
 $(window).on('load', function () {
-    preloader();
-    mainSlider();
-    aosAnimation();
-    wowAnimation();
+	preloader();
+	mainSlider();
+	aosAnimation();
+	wowAnimation();
 });
-
     // Made the left sidebar's min-height to window's height
     var winHeight = $(window).height();
     $('.dashboard-nav').css('min-height', winHeight);
@@ -581,6 +634,13 @@ $(window).on('load', function () {
     $(document).on('click', '.setting-button', function () {
         $('.option-panel').toggleClass('option-panel-collased');
     });
+$(window).on('load', function () {
+    preloader();
+    mainSlider();
+    aosAnimation();
+    wowAnimation();
+});
+
 });
 
 // mCustomScrollbar initialization
